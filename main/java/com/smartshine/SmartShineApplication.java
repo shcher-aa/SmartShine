e4ff466b994600de75d265864b8e5c58dbf56ebe
Anna <djepko3@gmail.com>
2025-06-21 11:58:49 +0200

    Update SmartShineApplication.java

--- main/java/com/smartshine/SmartShineApplication.java
+++ main/java/com/smartshine/SmartShineApplication.java
@@ -1,1 +1,11 @@
-// Пример содержимого файла SmartShine Java App
+// Пример содержимого файла SmartShine Java Apppackage com.smartshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartShineApplication.class, args);
+    }
+}
