0a1cff1588098f9177de90e894894c503c5e0584
Anna <djepko3@gmail.com>
2025-06-21 19:58:34 +0200

    Create ClientController.java

---
+++ src/main/java/com/smartshine/ClientController.java
@@ -0,0 +1,23 @@
+package com.smartshine;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/clients")
+public class ClientController {
+
+    private final ClientRepository clientRepository;
+
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping
+    public String listClients(Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        return "admin/clients";
+    }
+}
