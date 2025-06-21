8a8199fac3fabf9c4e70d80628aeff6fb27f9032
Anna <djepko3@gmail.com>
2025-06-21 16:06:38 +0200

    Update AppUser.java

--- src/main/java/com/smartshine/AppUser.java
+++ src/main/java/com/smartshine/AppUser.java
@@ -1,25 +1,52 @@
+package com.smartshine;
+
+import javax.persistence.*; // или jakarta.persistence.* если ты используешь Jakarta
+
 @Entity
 public class AppUser {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
+
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
     // Геттеры и сеттеры
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
