77bddc334b65dc2ec44660c22873b910bfb32970
Anna <djepko3@gmail.com>
2025-06-21 15:18:16 +0200

    Update AppUserRepository.java

--- src/main/java/com/smartshine/AppUserRepository.java
+++ src/main/java/com/smartshine/AppUserRepository.java
@@ -1,1 +1,8 @@
-// Пример содержимого файла SmartShine Java App
+package com.smartshine;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
+}
