34ec26188d9f6cb96ac5ced1ec50c8e83e309208
Anna <djepko3@gmail.com>
2025-06-21 15:22:14 +0200

    Create AppUserDetailsService.java

---
+++ src/main/java/com/smartshine/AppUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.smartshine;
+
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepository userRepository;
+
+    public AppUserDetailsService(AppUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+            .withUsername(user.getUsername())
+            .password(user.getPassword())
+            .roles("USER") // Или доставай роли из БД, если они есть
+            .build();
+    }
+}
