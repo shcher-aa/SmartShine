f915fac00bafa534d97f68e2b8014c8e822c86a7
Anna <djepko3@gmail.com>
2025-06-29 16:04:54 +0200

    Fix broken imports, missing POI dependency and repository classpaths

--- src/main/java/com/smartshine/repository/TaskRepository.java
+++ src/main/java/com/smartshine/repository/TaskRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findByAssignedEmployee(AppUser employee, Pageable pageable);
