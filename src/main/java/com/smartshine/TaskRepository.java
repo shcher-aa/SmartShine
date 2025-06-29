2d2766ba9d1406ade1696fb76beca4b022eaa0c3
Anna <djepko3@gmail.com>
2025-06-29 11:25:16 +0200

    Обновила таск репозиторийn

--- src/main/java/com/smartshine/TaskRepository.java
+++ src/main/java/com/smartshine/TaskRepository.java
@@ -4,15 +4,17 @@ import com.smartshine.model.Task;
 import com.smartshine.model.AppUser;
 import com.smartshine.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByAssignedEmployee(AppUser employee);
-    List<Task> findByDate(LocalDate date);
-    List<Task> findByAssignedEmployeeAndDate(AppUser employee, LocalDate date);
-    List<Task> findByAssignedEmployeeAndClientAndDate(AppUser employee, Client client, LocalDate date);
-    List<Task> findByClient(Client client);
-    List<Task> findByClientAndDate(Client client, LocalDate date);
+    Page<Task> findByAssignedEmployee(AppUser employee, Pageable pageable);
+    Page<Task> findByDate(LocalDate date, Pageable pageable);
+    Page<Task> findByAssignedEmployeeAndDate(AppUser employee, LocalDate date, Pageable pageable);
+    Page<Task> findByAssignedEmployeeAndClientAndDate(AppUser employee, Client client, LocalDate date, Pageable pageable);
+    Page<Task> findByClient(Client client, Pageable pageable);
+    Page<Task> findByClientAndDate(Client client, LocalDate date, Pageable pageable);
 }
