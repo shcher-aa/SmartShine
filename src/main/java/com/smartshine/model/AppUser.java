6da75e00a43bf228a925773f01f69b496d9bd432
Anna <djepko3@gmail.com>
2025-07-07 11:03:35 +0200

    Refactor: добавлен универсальный ClientController с разграничением по ролям

--- src/main/java/com/smartshine/model/AppUser.java
+++ src/main/java/com/smartshine/model/AppUser.java
@@ -1,7 +1,11 @@
 package com.smartshine.model;
 
-import jakarta.persistence.*; // 
-import com.smartshine.model.Role;
+import jakarta.persistence.Entity; //
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class AppUser {
